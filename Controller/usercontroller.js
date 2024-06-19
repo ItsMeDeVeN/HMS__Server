27d15e3529d655ba0fe7293cef07fc94994946a8
ItsMeDeVeN <devenbariya@gmail.com>
2024-06-19 16:19:53 +0530

    Forget Password Added

--- Controller/usercontroller.js
+++ Controller/usercontroller.js
@@ -57,11 +57,17 @@ const registerpatient = async (req, res) => {
   }
 };
 
-
 const registerdoctor = async (req, res) => {
   try {
-    const { name, email, password, confirmpassword, contact, department, ...rest } =
-      req.body;
+    const {
+      name,
+      email,
+      password,
+      confirmpassword,
+      contact,
+      department,
+      ...rest
+    } = req.body;
     if (!name) {
       return res
         .status(400)
@@ -118,10 +124,9 @@ const registerdoctor = async (req, res) => {
   }
 };
 
-
-const login = async(req, res) => {
-  try{
-    const{ email, password, ...rest} = req.body;
+const login = async (req, res) => {
+  try {
+    const { email, password, ...rest } = req.body;
 
     if (!email) {
       return res
@@ -134,57 +139,25 @@ const login = async(req, res) => {
         .send({ message: "Password is a compulsion!!", status: 400 });
     }
     const existinguser = await DoctorModel.findOne({ email });
-    if(existinguser){
-      try{
-      role = "Doctor";
-      if(existinguser.password !== password){
-        return res.status(403).send({
-          success: false,
-          message: "Wrong password Entered😵😵",
-          role: role,
-        });
-      }
-      const token = generateToken(existinguser._id, existinguser.name)
-      res.status(200).send({
-        success: true,
-        message: "Doctor Logged in succesfully 🥳🥳",
-        user: existinguser,
-        role: role,
-        token: token,
-      });
-    
-    }catch (error) {
-      res.status(500).send({
-        success: false,
-        message: "Error occured while logging",
-        errormsg: error,
-        role: role,
-      });
-      console.log(error);
-    }
-  }
-    else if(!existinguser){
-      const existinguser = await PatientModel.findOne({ email });
-      if(existinguser){
-        try{
-        role = "Patient";
-        if(existinguser.password !== password){
+    if (existinguser) {
+      try {
+        role = "Doctor";
+        if (existinguser.password !== password) {
           return res.status(403).send({
             success: false,
             message: "Wrong password Entered😵😵",
-            role: role
+            role: role,
           });
         }
-        const token = generateToken(existinguser._id, existinguser.name)
+        const token = generateToken(existinguser._id, existinguser.name);
         res.status(200).send({
           success: true,
-          message: "Patient Logged in succesfully 🥳🥳",
+          message: "Doctor Logged in succesfully 🥳🥳",
           user: existinguser,
           role: role,
-          token : token,
+          token: token,
         });
-      
-      }catch (error) {
+      } catch (error) {
         res.status(500).send({
           success: false,
           message: "Error occured while logging",
@@ -193,18 +166,89 @@ const login = async(req, res) => {
         });
         console.log(error);
       }
-    }
-      else{
+    } else if (!existinguser) {
+      const existinguser = await PatientModel.findOne({ email });
+      if (existinguser) {
+        try {
+          role = "Patient";
+          if (existinguser.password !== password) {
+            return res.status(403).send({
+              success: false,
+              message: "Wrong password Entered😵😵",
+              role: role,
+            });
+          }
+          const token = generateToken(existinguser._id, existinguser.name);
+          res.status(200).send({
+            success: true,
+            message: "Patient Logged in succesfully 🥳🥳",
+            user: existinguser,
+            role: role,
+            token: token,
+          });
+        } catch (error) {
+          res.status(500).send({
+            success: false,
+            message: "Error occured while logging",
+            errormsg: error,
+            role: role,
+          });
+          console.log(error);
+        }
+      } else {
         return res.status(404).send({
           success: false,
           message: "User not found.",
         });
       }
     }
-  }
-  catch{
+  } catch {}
+};
 
+const forgotpassword = async (req, res) => {
+  try {
+    const { email, newpassword, confirmnewpassword } = req.body;
+    
+    if (!email || !newpassword || !confirmnewpassword) {
+      return res.status(400).send({ 
+        message: "All fields are compulsory!", 
+        status: 400 
+      });
+    }
+
+    if (newpassword !== confirmnewpassword) {
+      return res.status(403).send({
+        success: false,
+        message: "Both passwords should match!!!"
+      });
+    }
+
+    const existingUser = await DoctorModel.findOne({ email }) || await PatientModel.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(404).send({
+        success: false,
+        message: "User not found.",
+      });
+    }
+
+    existingUser.password = newpassword;  
+    existingUser.confirmpassword = confirmnewpassword;
+    await existingUser.save();
+    return res.status(200).send({
+      success: true,
+      message: "Password updated successfully 🥳🥳!!!",
+      user: existingUser,
+    });
+
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Error occurred while updating password!!!",
+      errormsg: error.message,
+    });
+    console.error(error);
   }
-}
- 
-module.exports = { registerpatient, registerdoctor, login };
+};
+
+module.exports = { registerpatient, registerdoctor, login, forgotpassword};
--- routes/AllRoutes.js
+++ routes/AllRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { registerdoctor, registerpatient, login} = require('../Controller/usercontroller')
+const { registerdoctor, registerpatient, login, forgotpassword} = require('../Controller/usercontroller')
 const router = express.Router();
 
 router.post('/registerdoctor', registerdoctor)
 router.post('/registerpatient', registerpatient)
 router.post('/login', login)
+router.post('/forgotpassword', forgotpassword)
 // router.delete('/delete', deleteUser)
 // router.get('/alluser', getAllUser);
 // router.put('/updatedetails', updateDetails);
